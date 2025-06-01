49502f0591be69b9a9752a9e57372378f25f0a45
soin8293 <soin8293@colostate.edu>
2025-05-31 20:48:58 -0600

    The2

--- client/net.js
+++ client/net.js
@@ -1,7 +1,6 @@
 // net.js (ES module)
-// Colyseus Client is now exposed globally by index.html
-
-const client = new window.ColyseusClient("wss://gorilla-vs-humans-io.onrender.com");
+// client/net.js  (top of file)
+const client = new Colyseus.Client("wss://gorilla-vs-humans-io.onrender.com");
 window.colyseusRoom = null; // Expose room globally for game.js to send inputs
 
 // gameState will be populated by server updates and consumed by game.js
